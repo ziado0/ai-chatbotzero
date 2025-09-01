abac19699f638c089f7135abe413b09c73ba15ef
ziado0 <ziad.ab90@gmail.com>
2025-09-02 01:12:25 +0300

    Delete config.example.js

--- config.example.js
+++
@@ -1,6 +0,0 @@
-// Copy this file to config.js and add your API key for local development
-// DO NOT commit config.js to git - it's in .gitignore
-
-window.CONFIG = {
-    HUGGINGFACE_API_KEY: 'your_huggingface_token_here'
-};
