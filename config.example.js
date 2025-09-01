c33fabcf24d23cfb92bc98cf3e373e7b0754d18e
ziado0 <ziad.ab90@gmail.com>
2025-09-02 01:12:46 +0300

    Add files via upload

---
+++ config.example.js
@@ -0,0 +1,6 @@
+// Copy this file to config.js and add your API key for local development
+// DO NOT commit config.js to git - it's in .gitignore
+
+window.CONFIG = {
+    GROQ_API_KEY: 'your_groq_api_key_here'
+};
